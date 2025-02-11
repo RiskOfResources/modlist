34db2aa3ff4a7b757437a79b660078e2bf77cc17
Eric Lee <mail@ericlee.dev>
2025-02-11 15:53:21 -0500

    initial commit

---
+++ ModList/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using RoR2;
+using UnityEngine;
+
+namespace ModList
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+ 
+            On.RoR2.RunReport.Generate += RunReportOnGenerate;
+        }
+
+        private void OnDestroy() {
+            On.RoR2.RunReport.Generate -= RunReportOnGenerate;
+        }
+
+        private RunReport RunReportOnGenerate(On.RoR2.RunReport.orig_Generate orig, Run run, GameEndingDef gameending) {
+            if (!RoR2.UI.ConsoleWindow.instance) {
+                Instantiate(Resources.Load<GameObject>("Prefabs/UI/ConsoleWindow")).GetComponent<RoR2.UI.ConsoleWindow>();
+            }
+ 
+            Debug.Log("Loaded Plugins:");
+            foreach (var plugin in Chainloader.PluginInfos)
+            {
+                Debug.Log(plugin.Value.ToString());
+            }
+
+            return orig(run, gameending);
+        }
+    }
+}
