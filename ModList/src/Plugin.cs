a65fd7fc9f05fee45079b937e58e58f69fd9a68d
MarkTullius <mark_standerwick@live.com>
2025-02-12 00:03:48 +0000

    Prep for Thunderstore release and change mod printing to when the report is displayed to the player

---
+++ ModList/src/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using RoR2.UI;
+using UnityEngine;
+
+namespace ModList
+{
+  [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+  public class Plugin : BaseUnityPlugin
+  {
+    private void Awake()
+    {
+      // Plugin startup logic
+      Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+      On.RoR2.UI.GameEndReportPanelController.SetDisplayData += RunReportOnGenerate;
+    }
+
+    private void OnDestroy()
+    {
+      On.RoR2.UI.GameEndReportPanelController.SetDisplayData -= RunReportOnGenerate;
+    }
+
+    private void RunReportOnGenerate(On.RoR2.UI.GameEndReportPanelController.orig_SetDisplayData orig, GameEndReportPanelController self, GameEndReportPanelController.DisplayData newDisplayData)
+    {
+      if (!ConsoleWindow.instance)
+      {
+        Instantiate(Resources.Load<GameObject>("Prefabs/UI/ConsoleWindow")).GetComponent<ConsoleWindow>();
+      }
+
+      Debug.Log("Loaded Plugins:");
+      foreach (var plugin in Chainloader.PluginInfos)
+      {
+        Debug.Log(plugin.Value.ToString());
+      }
+
+      orig(self, newDisplayData);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mod List
+
+For use when Speedrunning, provides the same mod list verification as the Gauntlet mod without the requirement to have that mod installed. ![Spedrun](https://cdn.7tv.app/emote/01FJEWA1Z0000FZHS49NWQS41H/4x.webp)
